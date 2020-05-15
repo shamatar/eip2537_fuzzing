73110c044f422bbf85862e02b3e79ce963ea2c34
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-05-15 14:28:25 +0300

    run cargo update to fetch latest versions from git

--- libfuzzer_go/run_compare_ops.sh
+++ libfuzzer_go/run_compare_ops.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 cargo install cargo-fuzz
-# cd fuzz
-# cargo clean eth_pairings_go
-# cargo update
-# cd ..
+cd fuzz
+cargo update
+cd ..
 cargo +nightly fuzz run --release fuzz_target_compare_ops -- -max_len=16000 -jobs=1000 
 #-workers=32
